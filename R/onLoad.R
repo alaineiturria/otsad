cf415a4cc9aaa57a58248b06cfe927ad01f537e2
alaineiturria <38525253+alaineiturria@users.noreply.github.com>
2018-09-24 09:41:34 +0200

    ok

--- R/onLoad.R
+++
@@ -1,7 +0,0 @@
-CAD_OSE <- NULL
-.onLoad <- function(libname, pkgname){
-  CAD_OSE <<- reticulate::import_from_path("CAD_OSE", 
-                                           system.file("python", "CAD", 
-                                                       package = utils::packageName(), 
-                                                       mustWork = TRUE))
-}
