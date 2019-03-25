8bbecd98215a5319a607ea125f92011b7d9247eb
alaineiturria <38525253+alaineiturria@users.noreply.github.com>
2019-03-25 18:51:13 +0100

    dontest removed

--- tests/examples/contextual_ad_example.R
+++ tests/examples/contextual_ad_example.R
@@ -1,4 +1,3 @@
-\donttest{
 ## Generate data
 set.seed(100)
 n <- 200
@@ -14,5 +13,5 @@ result <- ContextualAnomalyDetector(data = df$value, rest.period = 10, base.thre
 ## Plot results
 res <- cbind(df, result$result)
 PlotDetections(res, title = "CAD_OSE ANOMALY DETECTOR")
-}
+
 
